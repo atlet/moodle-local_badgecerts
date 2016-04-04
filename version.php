829f1df07878ac2b6c4d1a8f210fcda40f1ecb54
Andraž <atletek@gmail.com>
2016-04-04 07:37:14 +0200

    Sem povečal št. verzije.

--- version.php
+++ version.php
@@ -21,7 +21,7 @@
  * @author     Gregor Anželj <gregor.anzelj@gmail.com>
  */
 
-$plugin->version = 2015042800;
+$plugin->version = 2016040400;
 $plugin->requires = 2013111800; // Moodle 2.6.0 or newer
 $plugin->component = 'local_badgecerts';
 $plugin->maturity = MATURITY_BETA;
